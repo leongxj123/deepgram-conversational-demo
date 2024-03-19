a6d291718bc8e1867b373f93338a263bd277eeb3
leongxj123 <113355197+leongxj123@users.noreply.github.com>
2024-03-19 20:18:35 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -23,18 +23,6 @@ export default function Home() {
         <div className="bg-gradient-to-b from-black/50 to-black/10 backdrop-blur-[2px] h-[4rem] flex items-center">
           <header className="mx-auto w-full max-w-7xl px-4 md:px-6 lg:px-8 flex items-center justify-between">
             <div className="flex items-center gap-6 text-sm">
-              <span className="mt-1">
-                <GitHubButton
-                  href="https://nb.2023gpt.top"
-                  data-color-scheme="no-preference: light; light: light; dark: light;"
-                  data-size="large"
-                  data-show-count="true"
-                  aria-label="get more"
-                >
-                  Star
-                </GitHubButton>
-              </span>
-
               <span className="gradient-shadow bg-gradient-to-r to-[#13EF93]/50 from-[#149AFB]/80 rounded">
                 <a
                   href="https://nb.2023gpt.top"
@@ -57,50 +45,6 @@ export default function Home() {
         <div className=" backdrop-blur-[2px] h-[4rem] flex items-center">
           <footer className="mx-auto w-full max-w-7xl px-4 md:px-6 lg:px-8 flex items-center justify-center gap-4 md:text-xl font-inter text-[#8a8a8e]">
             <span className="text-base text-[#4e4e52]">share it</span>
-            <a
-              href="#"
-              onClick={(e) => {
-                window.open(
-                  "https://nb.2023gpt.top",
-                  "",
-                  "_blank, width=600, height=500, resizable=yes, scrollbars=yes"
-                );
-
-                return e.preventDefault();
-              }}
-              target="_blank"
-            >
-              <XIcon className="mb-1" />
-            </a>
-            <a
-              href="#"
-              onClick={(e) => {
-                window.open(
-                  "https://nb.2023gpt.top",
-                  "",
-                  "_blank, width=600, height=500, resizable=yes, scrollbars=yes"
-                );
-
-                return e.preventDefault();
-              }}
-            >
-              <LinkedInIcon className="mb-1" />
-            </a>
-            <a
-              href="#"
-              onClick={(e) => {
-                window.open(
-                  "https://nb.2023gpt.top",
-                  "",
-                  "_blank, width=600, height=500, resizable=yes, scrollbars=yes"
-                );
-
-                return e.preventDefault();
-              }}
-              target="_blank"
-            >
-              <FacebookIcon className="mb-1" />
-            </a>
             <div className="border-l border-[#4e4e52] w-px h-7">&nbsp;</div>
             <a
               className="text-base font-semibold"
