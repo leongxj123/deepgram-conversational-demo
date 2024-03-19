8160da28bb1d7019bd35cdef6345811d8e48e89d
leongxj123 <113355197+leongxj123@users.noreply.github.com>
2024-03-19 13:18:05 +0800

    Update route.ts

--- app/api/brain/route.ts
+++ app/api/brain/route.ts
@@ -18,7 +18,7 @@ export async function POST(req: Request) {
   // Request the OpenAI API for the response based on the prompt
   try {
     const response = await openai.chat.completions.create({
-      model: "gpt-3.5-turbo-0125",
+      model: "gpt-3.5-turbo",
       stream: true,
       messages: messages,
     });
