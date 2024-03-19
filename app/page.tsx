910ef2bc6b617321e8d4563276de3598a116ad7f
leongxj123 <113355197+leongxj123@users.noreply.github.com>
2024-03-19 20:14:30 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -22,18 +22,6 @@ export default function Home() {
         {/* height 4rem */}
         <div className="bg-gradient-to-b from-black/50 to-black/10 backdrop-blur-[2px] h-[4rem] flex items-center">
           <header className="mx-auto w-full max-w-7xl px-4 md:px-6 lg:px-8 flex items-center justify-between">
-            <div>
-              <a className="flex items-center" href="/">
-                <Image
-                  className="w-auto h-8 max-w-[12.5rem] sm:max-w-none"
-                  src="/deepgram.svg"
-                  alt="Deepgram Logo"
-                  width={0}
-                  height={0}
-                  priority
-                />
-              </a>
-            </div>
             <div className="flex items-center gap-6 text-sm">
               <span className="mt-1">
                 <GitHubButton
