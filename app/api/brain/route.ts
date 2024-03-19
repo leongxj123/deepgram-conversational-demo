d14be26e9be9c2cb5dd37dce4a770ffdd2b6f365
leongxj123 <113355197+leongxj123@users.noreply.github.com>
2024-03-19 20:02:41 +0800

    Update route.ts

--- app/api/brain/route.ts
+++ app/api/brain/route.ts
@@ -18,7 +18,7 @@ export async function POST(req: Request) {
       },
       body: JSON.stringify({
         model: openaiApiModel,
-        stream: true,
+        stream: false,
         messages: messages,
       }),
     });
